acdf70938b75cf9f456133494ee7cd109dcd345b
ognyp <Oksankagnyp@gmail.com>
2024-01-27 16:36:06 +0200

    Added descr to promise work

--- tests/Homework9.js
+++ tests/Homework9.js
@@ -1,5 +1,6 @@
 //Train Promises
 
+
 //Promise func - such funcs will be delegated Wep API to process and js program will move on
 let fetchUrlData = function(url){
     return new Promise((res, rej) => {
@@ -10,11 +11,11 @@ let fetchUrlData = function(url){
 }
 
 
-//Key words: then/catch - will wait the result(resolved/rejected) of specified promise
+//Key words: then/catch - will get the returned result(resolved/rejected) of specified promise
 //first then will return promise, second then will return promise data
-//!!! JS do not support multythreading, so await/then will not stop the main thread, it will continue to executing code
-//written after await or then
-// and when await/then will get the data from promise - it will return it(((
+//!!! JS do not support multythreading, so await/then will not stop the main thread, it will continue to executing code,
+//written after await or then - result of promise will be hold in event loop and will be moved to stack for executing after 
+//last line of program code will be executed, promises result will be executing after all code from "main"
 let getPromdataUsingThen = fetchUrlData("https://jsonplaceholder.typicode.com/comments/1")
                            .then(data => data.json())
                            .then(data => console.log(data))
@@ -55,6 +56,7 @@ Promise.all(allHomeworkProms)
 //Such func will start to execute after all promises from arg array will have any value
 let execAllProms = async () => console.log(await Promise.allSettled(allHomeworkProms));
 execAllProms();
+
 
 
 
